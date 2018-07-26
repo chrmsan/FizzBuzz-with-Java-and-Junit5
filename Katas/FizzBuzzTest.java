785cebd68e8e4c22e1dad9725bef14930cfa8b1a
Christopher Sanchez <sanchez.christopherm@gmail.com>
2018-07-26 14:32:41 +0200

    Added new test that returns Fizz when input number is three

--- Katas/FizzBuzzTest.java
+++ Katas/FizzBuzzTest.java
@@ -9,12 +9,23 @@ import org.junit.jupiter.api.DisplayName;
 class FizzBuzzTest {
 
 	@Test
-	@DisplayName("Print input number")
+	@DisplayName("Returns input number as string")
 	void returnInput() {
 		FizzBuzz fizzbuzz = new FizzBuzz();
 		int input = 2;
 		String result = fizzbuzz.play(input);
 		String expectedResult = "2";
+		
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	@DisplayName("Returns 'Fizz' when input number is 3")
+	void returnFizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		int input = 3;
+		String result = fizzbuzz.play(input);
+		String expectedResult = "Fizz";
 		
 		assertEquals(expectedResult, result);
 	}
